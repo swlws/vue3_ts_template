64a6934bfadbc0a5624843571f22dd3431cd3c39
swlws <979623074@qq.com>
2021-05-12 17:58:34 +0800

    <feat>: 国际化

--- src/plugin/element.ts
+++ src/plugin/element.ts
@@ -1,6 +1,13 @@
 import { App } from "vue";
 import "element-plus/packages/theme-chalk/src/base.scss";
 
+import lang from "element-plus/lib/locale/lang/zh-cn";
+import "dayjs/locale/zh-cn";
+import locale from "element-plus/lib/locale";
+
+// 设置语言
+locale.use(lang);
+
 import {
   // ElAlert,
   // ElAside,
